d05983ae6c718d0d054a9d37d3233d9d2492d8a9
mekadalibrahem <mekadalibrahem123@gmail.com>
2024-07-19 00:11:40 +0300

    add config file with configration database connection

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php  
+return [
+
+    "database" =>[
+        'driver' => "mysql" ,
+        'dsn' => [
+            'host' => "127.0.0.1",
+            'port' => 3306,
+            "dbname" => "demo_app_db" ,
+            'charset' => 'utf8mb4'
+
+        ],
+        'username' => 'root',
+        'password' => ''
+    ],
+];
